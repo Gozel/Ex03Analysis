fe01d9df010a13fc4e9b8467fa1d785b7d0ccb50
Gözel Shakeri <goesel1709@gmail.com>
2017-04-18 17:04:10 +0100

    eye gaze data completely extracted

--- analyse_gaze_data.m
+++ analyse_gaze_data.m
@@ -2,7 +2,7 @@ header;
 
 gestures_data(:,11) = {0};
 
-for i = 3 : 3 %NO_PARTICIPANTS
+for i = 1 : NO_PARTICIPANTS
     
     % skipping participant 2 
     if 2 == i
@@ -13,7 +13,10 @@ for i = 3 : 3 %NO_PARTICIPANTS
 
         gaze = gaze_data{i, j};
         
-        time_stamp = experiment_data{i,j}{2,1};
+        start_time_stamp = experiment_data{i,j}{find(strcmp('VC start', string(experiment_data{i,j}{:,2})),1,'first'),1}; 
+        end_time_stamp = experiment_data{i,j}{find(strcmp('VC pause true', string(experiment_data{i,j}{:,2})),1,'last'),1}; 
+        
+        time_stamp_gaze = linmap(gaze{:,2}*1000, [start_time_stamp, end_time_stamp]);
         
         start_index = find(cell2mat(gestures_data(1:end-3,1)) == i ...
             & cell2mat(gestures_data(1:end-3,2)) == BLSQ(i,j), 1, 'first');
@@ -23,20 +26,16 @@ for i = 3 : 3 %NO_PARTICIPANTS
         for k = start_index : end_index
                 
             l = 1;
-            while (gaze{l,2} * 1000 + time_stamp) < gestures_data{k,8}
+            while time_stamp_gaze(l) < gestures_data{k,8}
                 l = l + 1;
             end
-            gaze_start = l;
+            gaze_start = l - 1;
             
-            while (gaze{l,2} * 1000 + time_stamp) < gestures_data{k,8} + gestures_data{k,9}
+            while time_stamp_gaze(l) < gestures_data{k,8} + gestures_data{k,9}
+                
                 l = l + 1;
             end
-            gaze_end = l;
-            
-%             gaze_start = find((gaze{:,2}+time_stamp) >= gestures_data{k,8}, 1, 'first');
-%             gaze_end = find((gaze{:,2}+time_stamp) <= (gestures_data{k,8} + gestures_data{k,9}), 1, 'last');
-
-            fprintf('i: %d, j: %d, k: %d, gaze start: %d, gaze end: %d\n', i, j, k, gaze_start, gaze_end);
+            gaze_end = l - 1;
             
             for l = gaze_start : gaze_end
               gestures_data{k,11} = gestures_data{k,11} + ifelse(0 == gaze{l,17}, gaze{l+1,2} - gaze{l,2}, 0);  
